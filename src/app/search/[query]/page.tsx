5dd4bff75c2f32a25d187ae8b332b90d1036930c
Rafa Canosa <rafacv23@gmail.com>
2024-10-30 15:03:48 +0100

    types

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -1,4 +1,13 @@
+import { Book } from "@/types/types"
 import Link from "next/link"
+
+interface BookApiResponse {
+  author_name: string[]
+  key: string
+  title: string
+  isbn: string
+  cover_i: string
+}
 
 export default async function SearchPage({
   params,
@@ -9,7 +18,7 @@ export default async function SearchPage({
   const response = await fetch(url)
   const data = await response.json()
 
-  const books = data.docs.map((book) => ({
+  const books: Book[] = data.docs.map((book: BookApiResponse) => ({
     title: book.title,
     author: book.author_name?.[0] || "Unknown Author",
     key: book.key.replace("/works/", "").replace("/books/", ""), // Extract key without prefix
@@ -20,17 +29,19 @@ export default async function SearchPage({
   console.log(books)
 
   return (
-    <div>
-      <h1>Search Page</h1>
-      <ul>
-        {books.map((book) => (
-          <li key={book.key}>
-            <Link href={`/book/${book.key}`}>
-              {book.title} by {book.author}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Search Page</h1>
+        <ul>
+          {books.map((book) => (
+            <li key={book.key}>
+              <Link href={`/book/${book.key}`}>
+                {book.title} by {book.author}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
