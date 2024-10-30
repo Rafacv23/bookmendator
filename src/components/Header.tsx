675c0bd9d5c2effe8a7c632bf9a3d669516ae816
Rafa Canosa <rafacv23@gmail.com>
2024-10-30 13:21:30 +0100

    new icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
 import { Button, buttonVariants } from "@/components/ui/button"
-import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components"
+import {
+  LoginLink,
+  LogoutLink,
+  RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs/components"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import {
+  Library,
+  LogIn,
+  LogOut,
+  MessageSquare,
+  Search,
+  UserPlus,
+} from "lucide-react"
 
 export default async function Header() {
   const { getUser } = getKindeServerSession()
@@ -10,17 +22,33 @@ export default async function Header() {
     <header className="row-start-1 flex gap-6 flex-wrap items-center justify-center">
       {user ? (
         <>
-          <Button>Chats</Button>
-          <Button>Search</Button>
-          <Button>Library</Button>
+          <Button>
+            <MessageSquare />
+            Chats
+          </Button>
+          <Button>
+            <Search />
+            Search
+          </Button>
+          <Button variant="outline">
+            <Library />
+            Library
+          </Button>
           <LogoutLink className={buttonVariants({ variant: "destructive" })}>
+            <LogOut />
             Logout
           </LogoutLink>
         </>
       ) : (
         <>
-          <LoginLink>Sign in</LoginLink>
-          <LoginLink>Log in</LoginLink>
+          <RegisterLink>
+            <UserPlus />
+            New user
+          </RegisterLink>
+          <LoginLink>
+            <LogIn />
+            Log in
+          </LoginLink>
         </>
       )}
     </header>
