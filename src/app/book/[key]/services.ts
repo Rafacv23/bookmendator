9997d7f9dbe0dd70ca7709c9a8d29bbf1ad56ae2
Rafa Canosa <rafacv23@gmail.com>
2024-10-31 14:56:04 +0100

    created

---
+++ src/app/book/[key]/services.ts
@@ -0,0 +1,5 @@
+// primero comprobamos si existe el libro en la base dedatos
+// si existe fetcheamos el libro desde nuestra propia api
+// si existe, devolvemos el libro
+// si no existe fetcheamos a la api de openlibrary
+// añadimos el libro a la base dedados
