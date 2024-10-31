dc5083487552c06cc387b9bb8041d9d754fc0667
Rafa Canosa <rafacv23@gmail.com>
2024-10-31 10:46:58 +0100

    created

---
+++ src/app/library/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="text-center font-bold text-3xl text-gray-900">
+          Welcome to Bookmendator
+        </h2>
+      </div>
+    </div>
+  )
+}
