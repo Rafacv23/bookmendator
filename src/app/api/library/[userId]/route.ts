170240183fa08a0a4a4703d5579c07f85c9f0bd3
Rafa Canosa <rafacv23@gmail.com>
2024-11-04 17:40:07 +0100

    function to retrieve the users library

---
+++ src/app/api/library/[userId]/route.ts
@@ -0,0 +1,56 @@
+import { SITE_URL } from "@/site/config"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+export async function GET(
+  request: Request,
+  { params }: { params: { userId: string } }
+) {
+  try {
+    // check if user exists
+    const user = await prisma.user.findUnique({
+      where: { id: params.userId },
+    })
+
+    if (!user) {
+      return new Response("User not found", { status: 404 })
+    }
+
+    // retrieve library of the user
+    const libraryEntries = await prisma.library.findMany({
+      where: { userId: params.userId },
+    })
+
+    if (!libraryEntries) {
+      return new Response("Library not found", { status: 404 })
+    }
+
+    const booksData = await Promise.all(
+      libraryEntries.map(async (entry) => {
+        const bookResponse = await fetch(`${SITE_URL}/api/book/${entry.bookId}`)
+
+        if (!bookResponse.ok) {
+          throw new Error(`Failed to fetch data for book ID: ${entry.bookId}`)
+        }
+
+        const bookData = await bookResponse.json()
+
+        return {
+          ...entry,
+          book: bookData,
+        }
+      })
+    )
+
+    return new Response(JSON.stringify(booksData), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    })
+  } catch (error) {
+    console.error(error)
+    return new Response("Error retrieving library", { status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
+}
