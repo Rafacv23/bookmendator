6dd38a5839528d0b05f312406e4cdbabe5c17907
Rafa Canosa <rafacv23@gmail.com>
2024-11-04 17:41:10 +0100

    page to show library of the user, retrieving info from the api

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,10 +1,26 @@
-export default function Page() {
+import { SITE_URL } from "@/site/config"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+export default async function Page() {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
+
+  const res = await fetch(`${SITE_URL}/api/library/${user.id}`)
+  const library = await res.json()
+
+  console.log(library)
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-4 py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="text-center font-bold text-3xl text-gray-900">
-          Welcome to Bookmendator
+        <h2 className="text-center font-bold text-3xl">
+          Welcome to Bookmendator this is your personal library Page
         </h2>
+        <ul>
+          {library.map((book) => (
+            <li key={book.bookId}>{book.bookId}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
