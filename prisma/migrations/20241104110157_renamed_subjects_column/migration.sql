09d5804d63632a4541e6f937dca5a450ec141d42
Rafa Canosa <rafacv23@gmail.com>
2024-11-04 12:02:59 +0100

    renamed subjects column

---
+++ prisma/migrations/20241104110157_renamed_subjects_column/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `genres` on the `Book` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Book" DROP COLUMN "genres",
+ADD COLUMN     "subjects" TEXT[];
