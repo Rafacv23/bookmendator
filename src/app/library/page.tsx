96eb5db694889d27080bdc2f90b940928b47e901
Rafa Canosa <rafacv23@gmail.com>
2024-11-04 17:44:19 +0100

    displays the books

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,5 +1,12 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import { SITE_URL } from "@/site/config"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import Link from "next/link"
 
 export default async function Page() {
   const { getUser } = getKindeServerSession()
@@ -18,7 +25,26 @@ export default async function Page() {
         </h2>
         <ul>
           {library.map((book) => (
-            <li key={book.bookId}>{book.bookId}</li>
+            <li key={book.bookId} className="flex items-center gap-4">
+              <Link href={`/book/${book.key}`}>
+                <Card className="hover:shadow-lg transition-all hover:scale-105 hover:cursor-pointer max-w-sm w-96 h-40">
+                  <CardHeader className="flex flex-row items-center gap-4">
+                    {book.book.cover && (
+                      <img
+                        loading="lazy"
+                        src={book.book.cover}
+                        alt={`${book.book.title} cover`}
+                        className="w-20 h-28 rounded object-cover aspect-auto"
+                      />
+                    )}
+                    <div className="space-y-2">
+                      <CardTitle>{book.book.title}</CardTitle>
+                      <CardDescription>{book.book.author}</CardDescription>
+                    </div>
+                  </CardHeader>
+                </Card>
+              </Link>
+            </li>
           ))}
         </ul>
       </div>
