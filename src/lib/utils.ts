5ab524611bd97036d8c4749fd72a501ad306b1f4
Rafa Canosa <rafacv23@gmail.com>
2024-11-05 16:48:55 +0100

    new function to convert strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatString(input: string | undefined): string {
+  // Split the string into words, capitalize the first letter of each word, and join them back
+  if (!input) return ""
+  const formatted = input
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
+
+  // Add ellipses at the end
+  return formatted
+}
