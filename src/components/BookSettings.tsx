394b7aa18a420d03f55b4009f16e2100d4a41888
Rafa Canosa <rafacv23@gmail.com>
2024-11-05 16:41:26 +0100

    removed form logic instead of a new component

--- src/components/BookSettings.tsx
+++ src/components/BookSettings.tsx
@@ -1,161 +1,37 @@
-"use client"
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/components/ui/form"
-import { Settings, MessageSquareMore } from "lucide-react"
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
+import { MessageSquareMore } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { CardFooter } from "@/components/ui/card"
-import { useState } from "react"
+import { BookReview, BookStatus } from "@prisma/client"
+import { Badge } from "./ui/badge"
+import BookSettingsForm from "./BookSettingsForm"
 
-const formSchema = z.object({
-  status: z.enum(["readed", "reading", "toRead", "dropped"], {
-    required_error: "You need to pick a status.",
-  }),
-  rating: z.enum(["like", "dislike", "unrated"], {
-    required_error: "You need to pick a rating.",
-  }),
-})
+interface BookSettingsProps {
+  bookId: string
+  libraryId?: number
+  bookStatus?: BookStatus
+  bookReview?: BookReview
+}
 
-export default function BookSettings() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      status: "toRead",
-      rating: "unrated",
-    },
-  })
-
-  const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false)
-
-  function onSubmit(data: z.infer<typeof formSchema>) {
-    console.log(data)
-    setIsDrawerOpen(false)
-  }
-
+export default function BookSettings({
+  bookId,
+  libraryId,
+  bookReview,
+  bookStatus,
+}: BookSettingsProps) {
   return (
-    <CardFooter className="flex justify-between">
-      <Drawer open={isDrawerOpen} onOpenChange={setIsDrawerOpen}>
-        <DrawerTrigger asChild>
-          <Button>
-            <Settings /> Edit
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent>
-          <div className="mx-auto w-full max-w-sm">
-            <DrawerHeader>
-              <DrawerTitle>Edit your book</DrawerTitle>
-              <DrawerDescription>
-                Set your daily activity goal.
-              </DrawerDescription>
-            </DrawerHeader>
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                  control={form.control}
-                  name="status"
-                  render={({ field }) => (
-                    <FormItem className="space-y-3">
-                      <FormLabel>Status</FormLabel>
-                      <FormControl>
-                        <RadioGroup
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                          className="flex flex-col space-y-1"
-                        >
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="readed" />
-                            </FormControl>
-                            <FormLabel>Readed</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="reading" />
-                            </FormControl>
-                            <FormLabel>Reading</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="toRead" />
-                            </FormControl>
-                            <FormLabel>In library</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="dropped" />
-                            </FormControl>
-                            <FormLabel>Dropped</FormLabel>
-                          </FormItem>
-                        </RadioGroup>
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="rating"
-                  render={({ field }) => (
-                    <FormItem className="space-y-3">
-                      <FormLabel>Rate the book</FormLabel>
-                      <FormControl>
-                        <RadioGroup
-                          onValueChange={field.onChange}
-                          defaultValue={field.value}
-                          className="flex flex-col space-y-1"
-                        >
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="like" />
-                            </FormControl>
-                            <FormLabel>Like</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="dislike" />
-                            </FormControl>
-                            <FormLabel>Dislike</FormLabel>
-                          </FormItem>
-                          <FormItem className="flex items-center space-x-3 space-y-0">
-                            <FormControl>
-                              <RadioGroupItem value="unrated" />
-                            </FormControl>
-                            <FormLabel>Unrated</FormLabel>
-                          </FormItem>
-                        </RadioGroup>
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <DrawerFooter>
-                  <Button type="submit">Save</Button>
-                  <DrawerClose asChild>
-                    <Button variant="outline">Cancel</Button>
-                  </DrawerClose>
-                </DrawerFooter>
-              </form>
-            </Form>
-          </div>
-        </DrawerContent>
-      </Drawer>
+    <CardFooter className="grid gap-4">
+      <div className="flex flex-row justify-between items-center">
+        <div className="flex gap-2">
+          <Badge variant={"outline"}>{bookReview}</Badge>
+          <Badge variant={"outline"}>{bookStatus}</Badge>
+        </div>
+        <BookSettingsForm
+          bookId={bookId}
+          libraryId={libraryId}
+          bookReview={bookReview}
+          bookStatus={bookStatus}
+        />
+      </div>
       <Button>
         <MessageSquareMore /> Leave a comment
       </Button>
