a4703e1c2034c46f8aabe11930ee5a061e5b4f6e
Rafa Canosa <rafacv23@gmail.com>
2024-11-05 13:28:17 +0100

    created loading state

--- src/components/BookButtons.tsx
+++ src/components/BookButtons.tsx
@@ -3,6 +3,7 @@
 import { Bookmark, BotMessageSquare, Share2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import addBookToLibrary from "@/app/book/[key]/services/addBookToLibrary"
+import { useState } from "react"
 
 interface BookButtonsProps {
   bookId: string
@@ -10,16 +11,27 @@ interface BookButtonsProps {
 }
 
 export default function BookButtons({ bookId, userId }: BookButtonsProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  const handleAddBookToLibrary = async () => {
+    setIsLoading(true)
+    try {
+      await addBookToLibrary({ bookId: bookId, userId: userId })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className="flex gap-2">
       <Button>
         Ask AI
         <BotMessageSquare />
       </Button>
-      <Button
-        onClick={() => addBookToLibrary({ bookId: bookId, userId: userId })}
-      >
-        Add to library
+      <Button disabled={isLoading} onClick={handleAddBookToLibrary}>
+        {isLoading ? "Adding..." : "Add to library"}
         <Bookmark />
       </Button>
       <Button>
