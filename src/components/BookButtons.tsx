45789637992d58ae04d6d75856b85657d68ee426
Rafa Canosa <rafacv23@gmail.com>
2024-11-05 13:47:47 +0100

    added delete button

--- src/components/BookButtons.tsx
+++ src/components/BookButtons.tsx
@@ -1,16 +1,22 @@
 "use client"
 
-import { Bookmark, BotMessageSquare, Share2 } from "lucide-react"
+import { Bookmark, BookmarkX, BotMessageSquare, Share2 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import addBookToLibrary from "@/app/book/[key]/services/addBookToLibrary"
 import { useState } from "react"
+import deleteBookFromLibrary from "@/app/book/[key]/services/deleteBookFromLibrary"
 
 interface BookButtonsProps {
   bookId: string
   userId: string
+  isBookInLibrary: boolean
 }
 
-export default function BookButtons({ bookId, userId }: BookButtonsProps) {
+export default function BookButtons({
+  bookId,
+  userId,
+  isBookInLibrary,
+}: BookButtonsProps) {
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const handleAddBookToLibrary = async () => {
@@ -24,16 +30,34 @@ export default function BookButtons({ bookId, userId }: BookButtonsProps) {
     }
   }
 
+  const handleRemoveBookFromLibrary = async () => {
+    setIsLoading(true)
+    try {
+      await deleteBookFromLibrary({ bookId: bookId, userId: userId })
+    } catch (error) {
+      console.error(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
     <div className="flex gap-2">
       <Button>
         Ask AI
         <BotMessageSquare />
       </Button>
-      <Button disabled={isLoading} onClick={handleAddBookToLibrary}>
-        {isLoading ? "Adding..." : "Add to library"}
-        <Bookmark />
-      </Button>
+      {isBookInLibrary ? (
+        <Button disabled={isLoading} onClick={handleRemoveBookFromLibrary}>
+          {isLoading ? "Removing..." : "Remove from your library"}
+          <BookmarkX />
+        </Button>
+      ) : (
+        <Button disabled={isLoading} onClick={handleAddBookToLibrary}>
+          {isLoading ? "Adding..." : "Add to library"}
+          <Bookmark />
+        </Button>
+      )}
       <Button>
         <Share2 />
       </Button>
