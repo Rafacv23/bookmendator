0527ea6c43e4fc19c327741d28b5d4edf9851133
Rafa Canosa <rafacv23@gmail.com>
2024-11-05 16:41:03 +0100

    new props

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -2,9 +2,13 @@ import BookCard, { BookCardProps } from "@/components/BookCard"
 import Container from "@/components/Container"
 import { SITE_URL } from "@/site/config"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { BookReview, BookStatus } from "@prisma/client"
 
 interface LibraryEntry {
   book: BookCardProps
+  id: number
+  bookReview: BookReview
+  bookStatus: BookStatus
 }
 
 export default async function Page() {
@@ -14,7 +18,12 @@ export default async function Page() {
   const res = await fetch(`${SITE_URL}/api/library/${user.id}`)
   const library = await res.json()
 
-  const books = library.map((entry: LibraryEntry) => entry.book)
+  const books = library.map((entry: LibraryEntry) => ({
+    ...entry.book,
+    libraryId: entry.id,
+    bookReview: entry.bookReview,
+    bookStatus: entry.bookStatus,
+  }))
 
   return (
     <Container>
@@ -30,6 +39,9 @@ export default async function Page() {
               author={book.author}
               cover={book.cover}
               isLibrary={true}
+              libraryId={book.libraryId}
+              bookStatus={book.bookStatus}
+              bookReview={book.bookReview}
             />
           </li>
         ))}
