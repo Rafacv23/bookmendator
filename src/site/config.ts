539c8b4092259626db5fd393a6b02de091398048
Rafa Canosa <rafacv23@gmail.com>
2024-11-06 15:28:01 +0100

    new constants

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,1 +1,7 @@
 export const SITE_URL = "http://localhost:3000"
+export const SITE_NAME = "Bookmendator"
+export const PORTFOLIO_URL = "https://rafacanosa.dev"
+export const AUTHOR = "Rafa Canosa"
+export const GITHUB_URL = "https://github.com/rafacv23/bookmendator"
+export const LINKEDIN_URL =
+  "https://www.linkedin.com/in/rafa-canosa-vallejo-6328a5194/"
