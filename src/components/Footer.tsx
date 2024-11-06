95cf4e3f6749927abd0a00c0fba068300f5f038d
Rafa Canosa <rafacv23@gmail.com>
2024-11-06 15:28:40 +0100

    new content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,53 @@
+import {
+  SITE_NAME,
+  PORTFOLIO_URL,
+  AUTHOR,
+  GITHUB_URL,
+  LINKEDIN_URL,
+} from "@/site/config"
+import { Github, Linkedin } from "lucide-react"
+import Link from "next/link"
+import { buttonVariants } from "./ui/button"
+
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      this is a footer
+    <footer className="flex flex-col items-center justify-center bg-card text-primary-content p-10">
+      <nav className="flex flex-col items-center gap-4 mb-4">
+        <h4 className="font-bold">{SITE_NAME}</h4>
+        <div className="grid grid-flow-col gap-4">
+          <Link
+            href={GITHUB_URL}
+            title="Github Repository"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            <Github />
+          </Link>
+          <Link
+            href={LINKEDIN_URL}
+            title="Linkedin Profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={buttonVariants({ variant: "outline" })}
+          >
+            <Linkedin />
+          </Link>
+        </div>
+      </nav>
+      <aside className="flex flex-col items-center gap-4">
+        <p>
+          Created by
+          <Link
+            href={PORTFOLIO_URL}
+            target="_blank"
+            title="rafacanosa.dev"
+            className={buttonVariants({ variant: "link" })}
+          >
+            {AUTHOR}.
+          </Link>
+        </p>
+      </aside>
     </footer>
   )
 }
