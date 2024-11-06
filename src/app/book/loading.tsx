4b376718d909e6a1cdc480140aae14c9083618d4
Rafa Canosa <rafacv23@gmail.com>
2024-11-06 13:01:24 +0100

    new loader

--- src/app/book/loading.tsx
+++ src/app/book/loading.tsx
@@ -4,31 +4,29 @@ import { Skeleton } from "@/components/ui/skeleton"
 export default function Loading() {
   return (
     <Container>
-      <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-start sm:justify-center">
+      <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-start sm:justify-center max-w-4xl">
         <Skeleton className="h-[280px] w-[180px] rounded shadow-xl" />
         <div className="flex flex-col gap-2">
           <Skeleton className="h-[40px] w-[385px]" />
           <Skeleton className="h-[28px] w-[385px]" />
           <Skeleton className="h-[24px] w-[385px]" />
-          <Skeleton className="h-[36px] w-[385px]" />
-        </div>
-      </div>
-      <div className="grid sm:grid-cols-2 gap-4">
-        <div>
-          <Skeleton className="rounded-xl max-w-sm w-96 h-64" />
-        </div>
-        <div>
           <ul className="flex flex-wrap gap-2">
-            {Array.from({ length: 3 }).map((_, index) => (
-              <li key={index}>
-                <Skeleton className="h-[25px] w-[167px]" />
+            {Array.from({ length: 5 }).map((_, index) => (
+              <li key={index} className="flex items-center gap-4">
+                <Skeleton className="h-[21px] w-[75px]" />
               </li>
             ))}
           </ul>
+          <Skeleton className="h-[36px] w-[501px]" />
         </div>
       </div>
-      <div>
-        <Skeleton className="w-96 h-64" />
+      <div className="grid gap-4">
+        <div className="max-w-4xl">
+          <Skeleton className="h-[300px] w-[500px]" />
+        </div>
+        <div className="max-w-4xl">
+          <Skeleton className="h-[300px] w-[500px]" />
+        </div>
       </div>
     </Container>
   )
