022b4438858608a820d3fc60079df33f196c9677
Rafa Canosa <rafacv23@gmail.com>
2024-11-06 15:28:17 +0100

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import Header from "@/components/Header"
 import { ThemeProvider } from "@/components/ThemeProvider"
+import Footer from "@/components/Footer"
 
 export const metadata: Metadata = {
   title: "Bookmendator",
@@ -24,6 +25,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
