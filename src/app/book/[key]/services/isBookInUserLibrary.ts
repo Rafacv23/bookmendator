1ebc1237062db70833d645a6698ee77834c27f2c
Rafa Canosa <rafacv23@gmail.com>
2024-11-06 15:28:11 +0100

    check if not user is received

--- src/app/book/[key]/services/isBookInUserLibrary.ts
+++ src/app/book/[key]/services/isBookInUserLibrary.ts
@@ -7,6 +7,11 @@ export default async function isBookInUserLibrary(
 ) {
   // Check if the book is already in the library
   const res = await fetch(`${SITE_URL}/api/library/${userId}`)
+
+  //check if the user is logged in
+  if (!userId) {
+    return false
+  }
   const library: Library[] = await res.json()
 
   return library.some((entry) => entry.bookId === bookId)
