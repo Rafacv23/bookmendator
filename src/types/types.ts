c7fe34b8a9cc8065679ebe6375d4c86e07c9dbfd
Rafa Canosa <rafacv23@gmail.com>
2024-11-07 12:14:05 +0100

    new type for comments

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,13 @@
+import { Comment } from "@prisma/client"
+
 export interface Book {
   key: string
   title: string
   author: string
   description: string
   cover: string
+}
+
+export interface Comments extends Comment {
+  user: { name: string }
 }
