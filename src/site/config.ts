a05e263d5cf529d3253f36de2c5dcf0adfda1e12
Rafa Canosa <rafacv23@gmail.com>
2024-11-14 12:03:32 +0100

    seo tags

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,5 +1,13 @@
 export const SITE_URL = "http://localhost:3000"
-export const SITE_NAME = "Bookmendator"
+export const SITE_NAME = "Bookmendation"
+export const HERO_SUBTITLE =
+  "Your Personal Library, Enhanced with AI-Driven Book Discovery"
+export const HERO_DESCRIPTION =
+  "Manage your library, explore new genres, and receive book recommendations based on your personal collection. All free, open-source, and offline-ready."
+export const SITE_TITLE =
+  "Bookmendation: AI-Powered Book Recommendations & Personal Library Manager"
+export const META_DESCRIPTION =
+  "Discover your next favorite book with Bookmendation! Get AI-driven, personalized book recommendations based on your library and reading preferences. Free, open-source, and offline-capable with cross-platform support—Bookmendation lets you manage your personal library and chat with an AI for recommendations anytime, anywhere."
 export const PORTFOLIO_URL = "https://rafacanosa.dev"
 export const AUTHOR = "Rafa Canosa"
 export const GITHUB_URL = "https://github.com/rafacv23/bookmendator"
