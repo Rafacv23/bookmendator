06b9d172da5cad111990d0d0bb95e4627e998d19
Rafa Canosa <rafacv23@gmail.com>
2024-11-14 13:34:46 +0100

    separate settings options into a client component

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,9 @@
 import Container from "@/components/Container"
+import SettingsPanel from "@/components/SettingsPanel"
 import { buttonVariants } from "@/components/ui/button"
 import {
   getKindeServerSession,
   LoginLink,
-  LogoutLink,
   RegisterLink,
 } from "@kinde-oss/kinde-auth-nextjs/server"
 import Link from "next/link"
@@ -35,16 +35,10 @@ export default async function SettingsPage() {
 
   return (
     <Container>
-      <h1>Hi {user.given_name} This is your settings page</h1>
-      <div>
-        <button>Delete your account</button>
-        <button>Change your library visibility</button>
-        <button>Delete your library</button>
-        <button>Delete your searchs history</button>
-        <button>Delete the data (All related to AI)</button>
-        <button>Set your favourite theme for the app</button>
-        <LogoutLink>Close session</LogoutLink>
-      </div>
+      <h1 className="text-xl font-semibold">
+        Hi {user.given_name}! This is your settings page
+      </h1>
+      <SettingsPanel />
     </Container>
   )
 }
