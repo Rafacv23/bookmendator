41def08d2e2b61a4ce3721cc4d83096bd6663d8a
Rafa Canosa <rafacv23@gmail.com>
2024-11-25 10:42:24 +0100

    chat logic wrapped into new client  component

---
+++ src/components/Chat.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import { useChat } from "@/hooks/useChat"
+import Container from "./Container"
+import { Badge } from "./ui/badge"
+import { Card } from "./ui/card"
+import { Button } from "./ui/button"
+import { SendHorizontal } from "lucide-react"
+import TextareaAutosize from "react-textarea-autosize"
+
+export default function Chat() {
+  const {
+    userMessage,
+    setUserMessage,
+    chatMessages,
+    isGenerating,
+    initProgress,
+    handleSendMessage,
+  } = useChat()
+
+  return (
+    <Container>
+      <div className="flex flex-col gap-8 max-w-4xl">
+        {chatMessages.map((msg, index) => (
+          <div
+            key={index}
+            className={`flex flex-col ${
+              msg.role === "user" ? "items-end" : "items-start"
+            } gap-2`}
+          >
+            <Badge>{msg.role === "user" ? "User" : "AI"}</Badge>
+            <Card className="p-4 rounded-md">{msg.content}</Card>
+          </div>
+        ))}
+        <div className="flex flex-row justify-between items-center gap-4 min-w-96">
+          <TextareaAutosize
+            value={userMessage}
+            onChange={(e) => setUserMessage(e.target.value)}
+            placeholder="Type your next question"
+            className="bg-card overflow-hidden resize-none flex min-h-[36px] w-full rounded-md border border-input px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
+          />
+          <Button
+            type="submit"
+            onClick={handleSendMessage}
+            disabled={isGenerating || initProgress !== ""}
+          >
+            Ask
+            <SendHorizontal />
+          </Button>
+        </div>
+      </div>
+      <div className="max-w-3xl flex flex-col items-center gap-4 mt-8 text-primary/80">
+        <small>Model: Llama-3.2</small>
+        <small>{initProgress}</small>
+      </div>
+    </Container>
+  )
+}
