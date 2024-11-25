dc33f8ef34f5450541ab6abbd87a647419bea490
Rafa Canosa <rafacv23@gmail.com>
2024-11-25 12:08:43 +0100

    new constants

--- src/site/config.ts
+++ src/site/config.ts
@@ -1,11 +1,11 @@
 export const SITE_URL = "http://localhost:3000"
-export const SITE_NAME = "bookmendator"
+export const SITE_NAME = "Bookmendator"
 export const HERO_SUBTITLE =
   "Your Personal Library, Enhanced with AI-Driven Book Discovery"
 export const HERO_DESCRIPTION =
   "Manage your library, explore new genres, and receive book recommendations based on your personal collection. All free, open-source, and offline-ready."
 export const SITE_TITLE =
-  "bookmendator: AI-Powered Book Recommendations & Personal Library Manager"
+  "Bookmendator: AI-Powered Book Recommendations & Personal Library Manager"
 export const META_DESCRIPTION =
   "Discover your next favorite book with bookmendator! Get AI-driven, personalized book recommendations based on your library and reading preferences. Free, open-source, and offline-capable with cross-platform support—Bookmendation lets you manage your personal library and chat with an AI for recommendations anytime, anywhere."
 export const PORTFOLIO_URL = "https://rafacanosa.dev"
